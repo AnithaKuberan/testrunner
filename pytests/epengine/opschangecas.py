a6efe977ac70e3a152071e36c6fd0ad26049e888
AnithaKuberan <41673804+AnithaKuberan@users.noreply.github.com>
2019-10-10 14:44:05 +0530

    Update opschangecas.py

--- pytests/epengine/opschangecas.py
+++ pytests/epengine/opschangecas.py
@@ -312,7 +312,7 @@ class OpsChangeCasTests(BucketConfig):
 
 
             # do a set meta based on the existing CAS
-            set_with_meta_resp = mc_active.setWithMeta(key, '123456789', 0, 0, 123, cas, 0)
+            set_with_meta_resp = mc_active.setWithMeta(key, '123456789', 0, 0, 123, cas, None)
 
 
             # check what get meta say
@@ -335,7 +335,7 @@ class OpsChangeCasTests(BucketConfig):
             self.assertTrue(max_cas == cas, '[ERROR]Max cas  is not equal to cas {0}'.format(cas))
 
             # and then mix in a set with meta
-            set_with_meta_resp = mc_active.setWithMeta(key, '123456789', 0, 0, 225, max_cas+1, 0)
+            set_with_meta_resp = mc_active.setWithMeta(key, '123456789', 0, 0, 225, max_cas+1, None)
             cas_post_meta = mc_active.getMeta(key)[4]
 
 
